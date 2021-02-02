b99b16a4b14df66e0b6157ee9cf8f9b8525d8f8e
donetian <donetian.petkov@gmail.com>
2021-02-02 08:01:19 +0000

    Main Change

--- main.cs
+++ main.cs
@@ -6,15 +6,6 @@ class MainClass {
   
   public static void Main (string[] args) {
 
-    int maxQuantity = int.Parse(Console.ReadLine());
-
-    Queue<int> orders = new Queue<int>( Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
-
-    
-
-
-    
-
     
   }
 }
